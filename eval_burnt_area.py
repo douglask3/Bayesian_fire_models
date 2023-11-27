c6cff24dc1c4ff28ac22ef9fe1ce7966168df256
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2023-11-27 07:25:12 +0000

    more burnt area eval

--- eval_burnt_area.py
+++ eval_burnt_area.py
@@ -1,17 +1,25 @@
 from eval_trends import *
 
-if __name__=="__main__":    
-    filenames_model = ["/hpc/data/d05/cburton/jules_output/u-cf137/GFDL-ESM2M/*.ilamb.*.nc",
-                      "/hpc/data/d05/cburton/jules_output/u-cf137/HADGEM2-ES/*.ilamb.*.nc",
-                      "/hpc/data/d05/cburton/jules_output/u-cf137/IPSL-CM5A-LR/*.ilamb.*.nc",
-                      "/hpc/data/d05/cburton/jules_output/u-cf137/MIROC5/*.ilamb.*.nc", 
-                      "/scratch/hadea/isimip3a/u-cc669_isimip3a_fire/20CRv3-ERA5_obsclim/jules-vn6p3_20crv3-era5_obsclim_histsoc_default_burntarea-total_global_monthly_1901_2021.nc"]
-
-    names_model = ["isimip2b-GFDL-ESM2M",
+if __name__=="__main__":  
+    dir_isimip3a = "/scratch/hadea/isimip3a/u-cc669_isimip3a_fire/"
+    dir_isimip2b = "/hpc/data/d05/cburton/jules_output/u-cf137/"
+    filenames_model = [dir_isimip3a + "20CRv3-ERA5_obsclim/jules-inferno-vn6p3_20crv3-era5_obsclim_histsoc_default_burntarea-total_global_monthly_1901_2021.nc",
+                       dir_isimip3a + "20CRv3_obsclim/jules-inferno-vn6p3_20crv3_obsclim_histsoc_default_burntarea-total_global_monthly_1901_2015.nc",
+                       dir_isimip3a + "20CRv3-W5E5_obsclim/jules-inferno-vn6p3_20crv3-w5e5_obsclim_histsoc_default_burntarea-total_global_monthly_1901_2019.nc",
+                       dir_isimip3a + "GSWP3-W5E5_obsclim/jules-inferno-vn6p3_gswp3-w5e5_obsclim_histsoc_default_burntarea-total_global_monthly_1901_2019.nc",
+                       dir_isimip2b + "GFDL-ESM2M/*.ilamb.*.nc",
+                       dir_isimip2b + "HADGEM2-ES/*.ilamb.*.nc",
+                       dir_isimip2b + "IPSL-CM5A-LR/*.ilamb.*.nc",
+                       dir_isimip2b + "MIROC5/*.ilamb.*.nc"]
+                       
+    names_model = ["isimip3a-20CRv3-ERA5_obsclim",
+                   "isimip3a-20CRv3_obsclim",
+                   "isimip3a-20CRv3-W5E5_obsclim",
+                   'isimip3a-era5-obsclim',
+                   "isimip2b-GFDL-ESM2M",
                    "isimip2b-HADGEM2-ES",
                    "isimip2b-IPSL-CM5A-LR",
-                   "isimip2b-MIROC5",
-                   'isimip3a-era5-obsclim']
+                   "isimip2b-MIROC5"]
     
     variable_model = 'burnt_area_gb'
 
@@ -30,8 +38,7 @@ if __name__=="__main__":
     obs_scale = [1.0, 1.0, 1.0/100.0]
 
     units = '1'
-    output_file = 'outputs/trend_burnt_area_metric_results'
-    output_maps = 'outputs/burnt_area/'
+    output_dir = 'outputs/burnt_area_new'
 
     region_type = 'gfed'
 
@@ -39,4 +46,4 @@ if __name__=="__main__":
                             filenames_observation, observations_names,
                             year_range, 
                             n_itertations, tracesID, mod_scale,  obs_scale, units,
-                            output_file, output_maps, region_type = region_type)
+                            output_dir, region_type = region_type)
