134cf14534a520a9329491ecd0b42c03208e6a4d
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2025-03-10 16:59:16 +0000

    removuing unused code

--- make_input/era5_nrt/cds_api_wind.py
+++ make_input/era5_nrt/cds_api_wind.py
@@ -96,51 +96,7 @@ def download_era5(year = 1940, months = range(13), area = [90, -180, -90, 180],
         # Apply constraints to crop the cube
         cropped_cube = cube.extract(iris.Constraint(latitude=lat_constraint, longitude=lon_constraint))
 
-        return cropped_cube
-        lon_grid, lat_grid = np.meshgrid(lons, lats)
-        
-        # Define WGS84 Lat-Lon coordinate system
-        latlon_cs = iris.coord_systems.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
-
-        # Assign to latitude and longitude coords
-        for coord_name in ["latitude", "longitude"]:
-            coord = cube.coord(coord_name)
-            coord.coord_system = latlon_cs
-
-        from affine import Affine
-
-        # Define the transform (Affine mapping from pixel to real-world coordinates)
-        res = 0.25  # Resolution in degrees
-        xmin, xmax = cube.coord('longitude').points[[0, -1]]
-        ymin, ymax = cube.coord('latitude').points[[0, -1]]
-        
-        transform = Affine.translation(xmin - res / 2, ymax + res / 2) * Affine.scale(res, -res)
-        
-        # Create the mask
-        mask = geometry_mask(
-            geometries=shapes.geometry,
-            transform=transform,
-            out_shape=(len(cube.coord('latitude').points), len(cube.coord('longitude').points)),
-            invert=True  # Keep cells inside the shape
-        )
-
-        # Apply the mask to the cube
-        cube.data = np.where(mask, cube.data, np.nan)
-
-        set_trace()
-        mask = geometry_mask(
-            shapes.geometry,
-            transform=cube.coord_system().as_cartopy_crs(),  # Ensure correct projection
-            out_shape=(len(lats), len(lons)),
-            invert=True  # We want True inside the shape
-        )
-        cube.data = np.where(mask, cube.data, np.nan)  # Masking non-overlapping cells
-       
-        set_trace()
-        #with zipfile.ZipFile(file, 'r') as zip_ref:
-        #    zip_ref.extractall(file[:-4])
-        set_trace()
-            
+        return cropped_cube            
     
     process_var("2m_temperature", "daily_maximum")
     set_trace()
@@ -149,17 +105,6 @@ def download_era5(year = 1940, months = range(13), area = [90, -180, -90, 180],
     process_var("2m_temperature", "daily_maximum")
     process_var("2m_dewpoint_temperature", "daily_minimum")
     process_var("volumetric_soil_water_layer_1", "daily_minimum")
-
-    #request["variable"] = [
-    #        "2m_dewpoint_temperature",
-    #        "volumetric_soil_water_layer_1"
-    #    ],
-    #request["daily_statistic"] = "daily_minimum"
-    
-
-    #client = cdsapi.Client()
-    #client.retrieve(dataset, request).download()
-    #client.retrieve(dataset, request, temp_dir + '/daily_minimum.zip')
 
 
 if __name__=="__main__":
