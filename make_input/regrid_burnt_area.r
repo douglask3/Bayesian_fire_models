c67d2417a12ede4433098ba3b4a8440bf48da5e1
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2025-03-17 21:50:39 +0000

    regeidding for 0p05 data (can be 0p05 i.e 0,05 or 0p25 or whatever)

--- make_input/regrid_burnt_area.r
+++ make_input/regrid_burnt_area.r
@@ -1,19 +1,23 @@
 library(terra)
 source("libs/add_date_time.r")
 
-dir = '../data/data/burnt_area/'
+dir = 'data/data/burnt_area/modis0p05/'
 files = list.files(dir, full.names = TRUE)
-eg_raster = rast('../data/wwf_terr_ecos_0p5.nc')
-res = 0.5
-temp_out = '../temp/burnt_area_no_meta.nc'
-file_out = '../data/data/driving_data/burnt_area.nc'
+eg_raster = rast('data/wwf_terr_ecos_0p5.nc')
+res = 0.05
+temp_out = 'temp/burnt_area_no_meta-0p05.nc'
+file_out = 'data/data/driving_data2425/burnt_area0p05.nc'
+
+out_extent = c(-15, 5, 49, 63)
 
 dat = rast(files)
 
 ext(dat) = c(-180, 180, -90, 90)
 fact = res/res(dat)
-dat = aggregate(dat, fact)
-dat = dat * fact[1] * fact[2] * 100000/cellSize(dat[[1]])
+if (any(fact) != 1) {
+    dat = aggregate(dat, fact)
+    dat = dat * fact[1] * fact[2] * 100000/cellSize(dat[[1]])
+}
 
 writeCDF(dat, temp_out, overwrite=TRUE)
 
