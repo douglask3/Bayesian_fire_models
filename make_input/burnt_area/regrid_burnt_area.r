565f7e5e778d0d505269a586006677d97462bb48
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2025-03-20 21:47:43 +0000

    regridding burnt area to region - in this case UK

--- make_input/burnt_area/regrid_burnt_area.r
+++ make_input/burnt_area/regrid_burnt_area.r
@@ -6,7 +6,7 @@ files = list.files(dir, full.names = TRUE)
 eg_raster = rast('data/wwf_terr_ecos_0p5.nc')
 res = 0.05
 temp_out = 'temp/burnt_area_no_meta-0p05.nc'
-file_out = 'data/data/driving_data2425/burnt_area0p05.nc'
+file_out = 'data/data/driving_data2425/burnt_area0p05-uk.nc'
 
 out_extent = c(-15, 5, 49, 63)
 
@@ -18,7 +18,7 @@ if (any(fact) != 1) {
     dat = aggregate(dat, fact)
     dat = dat * fact[1] * fact[2] * 100000/cellSize(dat[[1]])
 }
-
+dat = crop(dat, out_extent)
 writeCDF(dat, temp_out, overwrite=TRUE)
 
 dates = substr(sapply(files, function(i) strsplit(i,'C6_')[[1]][2]), 1, 6)
