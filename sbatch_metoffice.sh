b87b9b87328f77555d814573b119f6e46a1138cf
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-04-11 15:31:48 +0100

    failed attemp at getting to work in new supercomputer

--- sbatch_metoffice.sh
+++ sbatch_metoffice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --qos=long  #high #long
-#SBATCH --mem=100000  # 100000
-#SBATCH --ntasks-per-node=1            # Number of tasks per node
-#SBATCH --nodes=1                      # Number of nodes
+#SBATCH --mem=100M
+#SBATCH --ntasks=5                     # Number of nodes
 #SBATCH --output=outputs/output_%j.txt         # Output file (%j expands to job ID)
 #SBATCH --error=outputs/error_%j.txt           # Error file (%j expands to job ID)
-#SBATCH --time=24:00:00 
+#SBATCH --time=10 
 
-conda activate pymc5_env
+conda activate bayesian-fire-models
 python run_ConFire.py
+
+
