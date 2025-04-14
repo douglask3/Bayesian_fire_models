47e99c067026566da89dd84a978d6cde1aded6de
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-04-14 13:30:20 +0100

    fixing bug by using raster instead of terra for final write

--- libs/add_date_time.r
+++ libs/add_date_time.r
@@ -22,12 +22,13 @@ add_date_to_file <- function(file, file_out = file, years, mnn, day, name = NULL
     dy = as.character(day)
     dy[day < 10] = paste0('0', dy[day < 10])
     date = as.Date(paste(years, mn, dy, sep = '-'))
-    
+   
     names(dat) = date
     time(dat) = date
     #dat = setZ(dat, as.Date(date), 'Date')
     if (!is.null(name)) varnames(dat) = name
     if (!is.null(longname)) longnames(dat) = longname
     
-    writeCDF(dat, file_out, overwrite = TRUE, ...)
+     
+    raster::writeRaster(brick(dat), file_out, overwrite = TRUE, ...)
 }
