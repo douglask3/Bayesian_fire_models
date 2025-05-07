5dd9383f367a86f359d1fd344fa325e3a6142983
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-05-07 10:55:23 +0100

    running for all Amazon files

--- make_input/isimip/landuse_change.py
+++ make_input/isimip/landuse_change.py
@@ -34,10 +34,35 @@ def work_out_change(dir, file_in, file_out):
 
 
 if __name__=="__main__":
-    dir = "data/data/driving_data2425/Amazon/isimp3a/obsclim/GSWP3-W5E5/period_2000_2019/"
-    files_in = ["debiased_tree_cover_jules-es.nc","crop_jules-es.nc",  "pasture_jules-es.nc"]
+
+    clims = ["obsclim", "counterclim"]
+    periods = ["period_2000_2019", "period_1901_1920"]
+
+    files_in  = ["debiased_tree_cover_jules-es.nc","crop_jules-es.nc",  
+                 "pasture_jules-es.nc"]
     files_out = ["debiased_tree_cover_change_jules-es.nc",
                  "crop_change_jules-es.nc", "pasture_change_jules-es.nc"]
+    
+    def run_for_all_files(dir):
+        return [work_out_change(dir, file_in, file_out) \
+                for file_in, file_out in zip(files_in, files_out)]
 
-    [work_out_change(dir, file_in, file_out) for file_in, file_out in zip(files_in, files_out)]
+    for clim in clims:
+        for period in periods:
+            dir = "data/data/driving_data2425/Amazon/isimp3a/" + clim + "/GSWP3-W5E5/" + \
+                  period + "/"
+        
+            run_for_all_files(dir)
 
+    experiments = ["historical", "ssp370", "ssp585",  "ssp126"]
+    models = ["GFDL-ESM4", "IPSL-CM6A-LR", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL"]
+    periods = ["period_1994_2014", "period_2015_2099", "period_2015_2099", "period_2015_2099"]
+    for experiment in experiments:
+        for model in models:
+            for period in periods:
+                dir = "data/data/driving_data2425/Amazon/isimp3b/" + experiment + "/" + \
+                      model + "/" + period + "/"
+                try:
+                    run_for_all_files(dir)
+                except:
+                    print("not run, likely no dir:" + dir)
