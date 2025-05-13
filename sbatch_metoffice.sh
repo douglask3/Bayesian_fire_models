3c2f67dc6d4ec469de17fdd4e561ecd94a859a53
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-05-13 22:35:41 +0100

    extending time

--- sbatch_metoffice.sh
+++ sbatch_metoffice.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=5                     # Number of nodes
 #SBATCH --output=outputs/ConFLAME_output_%j.txt         # Output file (%j expands to job ID)
 #SBATCH --error=outputs/ConFLAME_error_%j.txt           # Error file (%j expands to job ID)
-#SBATCH --time=4:00:00 
+#SBATCH --time=6:00:00 
 
 conda activate bayesian-fire-models
 python run_ConFire.py
