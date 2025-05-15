a84a8b23c33d414cb9a317feadac471c8c8bfeed
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-05-15 10:32:42 +0100

    upping memory

--- sbatch_metoffice.sh
+++ sbatch_metoffice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --mem=30000M
+#SBATCH --mem=150000M
 #SBATCH --ntasks=5                     # Number of nodes
 #SBATCH --output=outputs/ConFLAME_output_%j.txt         # Output file (%j expands to job ID)
 #SBATCH --error=outputs/ConFLAME_error_%j.txt           # Error file (%j expands to job ID)
