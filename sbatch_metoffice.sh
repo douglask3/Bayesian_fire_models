e7f408ea9840ad6320bb5895a73868472910a631
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-05-23 19:36:41 +0100

    impl,enting the namelist from command lien here too

--- sbatch_metoffice.sh
+++ sbatch_metoffice.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -l
 #SBATCH --mem=200000M
-#SBATCH --ntasks=5                     # Number of nodes
+#SBATCH --ntasks=10                     # Number of nodes
 #SBATCH --output=outputs/ConFLAME_output_%j.txt         # Output file (%j expands to job ID)
 #SBATCH --error=outputs/ConFLAME_error_%j.txt           # Error file (%j expands to job ID)
 #SBATCH --time=6:00:00 
 
+# Get the namelist argument
+NAMELIST=$1
+
 conda activate bayesian-fire-models
-python run_ConFire.py
+python run_ConFire.py  "$NAMELIST"
 
 
