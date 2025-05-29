a5d0b4366c5af98ce7506ade0523d08d5847f9d1
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-05-29 13:45:37 +0100

    doing kde for both types of plot

--- src/plot_attribution_scatter.py
+++ src/plot_attribution_scatter.py
@@ -1,30 +1,23 @@
-
+import xarray as xr
 from pdb import set_trace
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
-
-SoW_gradient_adjusted = [
-    #"#ffffff",
-    #"#e0efff",
-    "#cfe9ff",  # very light blue
-    #"#adddfb",  # slightly more saturated
-    #"#8fcff4",  # deeper blue
-    "#fbc585",  # lighter orange (closer to #fc6)
-    "#fc6",     # original orange
-    "#f9a17e",  # between orange and salmon
-    "#f68373",  # salmon
-    "#da4f58",  # halfway to deep red
-    "#c7384e",  # deep red
-    "#9b3049",  # deeper
-    "#862976",  # final purple
-    "#431533",
-    "#030001"
-]
-sow_cmap = LinearSegmentedColormap.from_list("SoW", SoW_gradient_adjusted)
+import state_of_wildfires_colours
+import seaborn as sns
+import sys
+sys.path.append('.')
+sys.path.append('src/')
+from state_of_wildfires_colours  import SoW_cmap
 
 # Load the data
+
+def plot_kde(x, y, xlab, ylab, cmap_name = "gradient_hues_extended", *args, **kw):
+    df = pd.DataFrame({xlab: x, ylab: y})
+    sns.kdeplot(data=df, x=xlab, y=ylab, fill=True, 
+                cmap=SoW_cmap[cmap_name], *args, **kw)
+
+
 
 factual = pd.read_csv("outputs/outputs/ConFire_nrt4Amazon-2425-fuel2/time_series/_19-frac_points_0.5/baseline-/mean/points-Evaluate.csv")
 
@@ -33,21 +26,16 @@ ix = [i for i in range(factual.shape[1]) if i % 12 in [1, 2, 3]]
 # Flatten the arrays to 1D
 factual_flat = factual.values[:,ix].flatten() + 0.000000001
 counterfactual_flat = counterfactual.values[:,ix].flatten() + 0.000000001 #[:,12:]
-import seaborn as sns
+
 # Scatter plot 1: Factual vs Counterfactual
 plt.figure(figsize=(6, 6))
-df = pd.DataFrame({
-    "factual": factual_flat,
-    "counterfactual": counterfactual_flat
-})
-log_levels = np.geomspace(1e-10, 1.0, 500)
-sns.kdeplot(data=df, x="factual", y="counterfactual", fill=True, cmap=sow_cmap, 
-    levels=log_levels, log_scale = True, thresh=1e-4)
 
-
-#plt.scatter(factual_flat, counterfactual_flat, alpha=0.3, s=5)
-#plt.hist2d(factual_flat, counterfactual_flat, bins=100, cmap='viridis', norm=plt.matplotlib.colors.LogNorm())
-
+#log_levels = np.geomspace(1e-10, 1.0, 500)
+# More bias to lower densities (power < 1)
+x = np.linspace(0, 1, 20)
+log_levels = x**(8)  # try 3, 5, 7 for increasingly strong bias
+plot_kde(factual_flat, counterfactual_flat, "factual", "counterfactual",
+         levels=log_levels, log_scale = True, thresh=1e-4)
 
 plt.plot([0.0000000000001, 100], [0.0000000000001, 100], 'k--', label='1:1 Line')
 plt.ylabel("Counterfactual Burned Area")
@@ -56,24 +44,16 @@ plt.title("Factual vs Counterfactual Burned Area")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-set_trace()
 
 # Avoid division by zero
 counterfactual_flat_safe = counterfactual_flat.copy()
-#counterfactual_flat_safe[counterfactual_flat_safe == 0] = 1e-16
 
 # Scatter plot 2: Factual vs Factual/Counterfactual
 effect_ratio = (factual_flat - counterfactual_flat)*100
 effect_ratio[effect_ratio>0] = effect_ratio[effect_ratio>0]/factual_flat[effect_ratio>0]
 effect_ratio[effect_ratio<0] = effect_ratio[effect_ratio<0]/counterfactual_flat[effect_ratio<0]
 
-plt.figure(figsize=(6, 6))
-plt.scatter(factual_flat, effect_ratio, alpha=0.3, s=5, color = 'red')
-plt.axhline(0, color='k', linestyle='--')#, label='No Change (Ratio = 1)')
 
-import xarray as xr
-import numpy as np
-import matplotlib.pyplot as plt
 
 # Load NetCDF
 ds = xr.open_dataset("data/data/driving_data2425/Amazon/burnt_area.nc")
@@ -91,12 +71,30 @@ else:
     jfm_ba = jfm_ba.max(axis=1)*10  # mean over JFM
 
 # Mean burned area during JFM
-set_trace()
 jfm_mean_value = jfm_ba.mean(axis = (1,2))[-3:].mean().item()#*100  # Get scalar
+
+plt.figure(figsize=(6, 6))
+x = np.linspace(0, 1, 20)
+log_levels = x**(5)  # try 3, 5, 7 for increasingly strong bias
+#log_levels = np.append(log_levels, np.array([0.5, 1.0]))
+xmin = factual_flat.max()/100
+test = factual_flat > xmin
+#plot_kde(factual_flat, effect_ratio, "factual", "effect ratio",
+#         levels=log_levels, thresh=1e-4, bw_adjust = 2, 
+#         clip=((0, jfm_mean_value), (-100, 100))) #, log_scale = True
+plot_kde(factual_flat[test], effect_ratio[test], "factual", "effect ratio",bw_adjust = 1,
+         levels=log_levels, thresh=1e-4, clip=((0.0,factual_flat.max()*1.3), (-100, 100))) #,  = True
+
+plot_kde(factual_flat[test], effect_ratio[test], "factual", "effect ratio",bw_adjust = 1,
+         levels=log_levels, thresh=1e-4, clip=((jfm_mean_value,factual_flat.max()*1.3), (-100, 100)))
+plt.axhline(0, color='k', linestyle='--')#, label='No Change (Ratio = 1)')
+
+
+
 plt.axvline(jfm_mean_value, color='red', linestyle='--', label='Observed Burned Area')
 
 mask = factual_flat > jfm_mean_value
-percentile = [5, 10, 50, 90, 95]
+percentile = [10, 50, 90]
 cc_effect = np.percentile(effect_ratio[mask], percentile)/100.0
 cc_effect = np.round(1.0+cc_effect/(1.0-cc_effect), 2)
 rr = np.mean(effect_ratio[mask]>0)
@@ -105,10 +103,10 @@ rr = np.round(rr/(1-rr), 2)
 percentile_text = [str(pc) + "%:\n" + str(cc) for pc, cc in zip(percentile, cc_effect)]
 
 ax = plt.gca()
-plt.text(0.45, 0.23, "Climate change impact (pvalue: " + str(pv) + ")", transform=ax.transAxes)
-for i in range(len(percentile_text)): plt.text(0.45 + i*0.10, 0.13, percentile_text[i], transform=ax.transAxes)
-plt.text(0.45, 0.09, "Risk Ratio:", transform=ax.transAxes)
-plt.text(0.45, 0.05, rr, transform=ax.transAxes)
+plt.text(0.55, 0.23, "Climate change impact (pvalue: " + str(pv) + ")", transform=ax.transAxes)
+for i in range(len(percentile_text)): plt.text(0.55 + i*0.10, 0.13, percentile_text[i], transform=ax.transAxes)
+plt.text(0.55, 0.09, "Risk Ratio:", transform=ax.transAxes)
+plt.text(0.55, 0.05, rr, transform=ax.transAxes)
 
 plt.xlabel("Factual Burned Area")
 plt.ylabel("Factual / Counterfactual Ratio")
