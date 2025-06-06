eacfc395278f303add2d4442cfaf8ff2619374bb
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-06-06 14:46:32 +0100

    adding common directory name

--- SoW_info/state_of_wildfires_region_info.py
+++ SoW_info/state_of_wildfires_region_info.py
@@ -1,3 +1,4 @@
+from pdb import set_trace
 Amazon_long_name = "Amazon forest northeast of the Amazon and Rio Negro rivers"
 LA_long_name = "Mediterranean portion of Los Angeles, Orange County, Ventura, San Bernardino, Riverside, San Diego"
 
@@ -5,22 +6,28 @@ LA_long_name = "Mediterranean portion of Los Angeles, Orange County, Ventura, Sa
 # Includes long and short display names, analysis years, and months of interest.
 regions_2324 = {"Congo"   : {"longname": "Central Congo basin forests", 
                              "shortname": "Congo Basin", 
-                             "years": [2024], "mnths":  ['07']},
+                             "years": [2024],  "mnths":  ['07'],
+                             "dir": "Congo"},
                 "Amazon"  : {"longname": Amazon_long_name, 
                              "shortname":  "Northeast Amazon", 
-                             "years": [2024], "mnths":  ['01', '02', '03']},
+                             "years": [2024], "mnths":  ['01', '02', '03'],
+                             "dir": "Amazon"},
                 "LA"      : {"longname": LA_long_name, 
                              "shortname":  "Southern California", 
-                             "years": [2025], "mnths":  ['01']},
+                             "years": [2025], "mnths":  ['01'],
+                             "dir": "Amazon"},
                 "Pantanal": {"longname": "Greater Pantanal basin plus Chiquitano forests", 
                              "shortname":  "Pantanal and Chiquitano",
-                             "years": [2024], "mnths":  ['08', '09']},
+                             "years": [2024], "mnths":  ['08', '09'],
+                             "dir": "Pantanal"},
                 "Alberta" : {"longname": "Alberta Mountain forests including Jasper NP", 
                              "shortname":  "Alberta", 
-                             "years": [2024], "mnths":  ['07']},
+                             "years": [2024], "mnths":  ['07'],
+                             "dir": "Alberta"},
                 "NEIndia" : {"longname": "Forests of northeast India, Nepal, Bangladesh", 
                              "shortname":  "Himalayan Foothills ", 
-                             "years": [2024], "mnths":  ['04']}}
+                             "years": [2024], "mnths":  ['04'],
+                             "dir": "NEIndia"}}
 
 def get_region_info(region_names, regions_info = regions_2324):
     """
@@ -48,5 +55,7 @@ def get_region_info(region_names, regions_info = regions_2324):
                 'years': [2024],
                 'mnths': ['01', '02', '03']}}
     """
-    if ~isinstance(region_names, list): region_names = [region_names]
+    
+    if not isinstance(region_names, list): region_names = [region_names]
+    
     return {key: regions_info[key] for key in region_names if key in regions_info}
