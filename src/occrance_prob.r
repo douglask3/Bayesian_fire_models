f06e8b99ecdf316613035c5c3234fc55dafa9a71
Douglas Kelley <douglas.i.kelley@gmail.com>
2025-06-21 19:57:48 +0100

    better out of range sampling and fast occurance calculataion

--- src/occrance_prob.r
+++ src/occrance_prob.r
@@ -1,3 +1,4 @@
+library(poibin)
 dir = 'Amazon/'
 event = 0.526584627
 ssps = c('historical', 'ssp126', 'ssp370', 'ssp585')
@@ -5,11 +6,12 @@ ssps = c('historical', 'ssp126', 'ssp370', 'ssp585')
 models = c("GFDL-ESM4-", "IPSL-CM6A-LR-", 
 		   "MPI-ESM1-2-HR-", "MRI-ESM2-0-", "UKESM1-0-LL-"	)
 		   
-exactly_n_events <- function(pevent, n) {
-	combs <- combn(length(pevent), n, simplify = FALSE)
-	sum(sapply(combs, function(idx) {
-		prod(pevent[idx]) * prod(1 - pevent[-idx])
-	}))
+atleast_n_events <- function(pevent, n) {
+	1 - ppoibin(n - 1, pevent)
+#	combs <- combn(length(pevent), n, simplify = FALSE)
+#	sum(sapply(combs, function(idx) {
+#		prod(pevent[idx]) * prod(1 - pevent[-idx])
+#	}))
 }
 
 cal_occurance_prob_for_ssp <- function(ssp, model, dir,
@@ -19,6 +21,7 @@ cal_occurance_prob_for_ssp <- function(ssp, model, dir,
 	print(ssp)
 	print(model)
 	print(nevents)
+	
 	before25 = substr(colnames(dat), 2, 5) < 2025
 	if (is.na(event))
 		event =  quantile(as.matrix(dat[,before25]), 1-1/12000)
@@ -29,32 +32,35 @@ cal_occurance_prob_for_ssp <- function(ssp, model, dir,
 	if (length(pevent) < (nyrs * 12)) {
 		if (whichend == 'end') {
 			pevent = c(pevent, 
-					   rep(tail(pevent, 1), 
-					       (nyrs * 12) - length(pevent)))
+					   rep(tail(pevent, 13), 
+					       length.out = (nyrs * 12) - length(pevent)))
 		} else {
-			pevent = c(rep(pevent[1], (nyrs * 12) - length(pevent)),
-					   pevent)
+			pevent = rep(pevent, length.out = (nyrs * 12))
 		}
 	}	
 	if (nevents == 1) {
 		out = 1-prod(1-pevent)
 	} else {
-		out = exactly_n_events(pevent, nevents)
+		out = atleast_n_events(pevent, nevents)
 	}
-	return(c(out,event))
+	return(c(event, out))
 }
 
 cal_occurance_prob_for_model <- function(event = NaN, ...) {
+	
 	houts = cal_occurance_prob_for_ssp(ssps[1], whichend = 'start', 
 								       event = event, ...)
-	event = houts[2]
+	event = houts[1]
 	
 	fouts = sapply(ssps[-1], cal_occurance_prob_for_ssp, event = event, ...)
-	return(c(houts, fouts[1,]))
+	
+	return(c(houts, fouts[2,]))
 }
 
 outs1 = sapply(models, cal_occurance_prob_for_model, dir, event = event)
 outs2 = sapply(models, cal_occurance_prob_for_model, dir, 
 			event = event, nevents = 2)
-#outs3 = sapply(models, cal_occurance_prob_for_model, dir, 
-#			   event = event, nevents = 3)
+outs3 = sapply(models, cal_occurance_prob_for_model, dir, 
+			   event = event, nevents = 3)
+			   
+		   
