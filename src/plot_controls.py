eac54607692d1207d5b620960b1858a457c46429
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-06-25 11:17:36 +0100

    plotting control anomolies

--- src/plot_controls.py
+++ src/plot_controls.py
@@ -16,7 +16,12 @@ from plot_maps import *
 
 
 # Function to load and concatenate all files in a directory
-def load_ensemble(directory):
+def load_ensemble(base_dir, run_dir, exp):
+    if isinstance(run_dir, list):
+        out = load_ensemble(base_dir, run_dir[0], exp)
+        out.data -= load_ensemble(base_dir, run_dir[1], exp).data
+        return out
+    directory = os.path.join(base_dir + '/' + run_dir + '/', exp)
     file_paths = sorted(glob.glob(os.path.join(directory, "sample-pred*.nc")))
     skips = max([1, round(len(file_paths)/max_smaples)])
     file_paths = file_paths[::skips]
@@ -56,39 +61,50 @@ def plot_map(cube, title, cmap, levels, ax):
 
 from pdb import set_trace
 # Define the base directory
-base_dir = "outputs/outputs_scratch/ConFLAME_nrt-attribution8/Amazon-2425//samples/_19-frac_points_0.5//baseline-/"
-
+base_dir = "outputs/outputs_scratch/ConFLAME_nrt-attribution9/Amazon-2425//samples/_19-frac_points_0.5//"
+run_dirs = ["factual-", "counterfactual-"] 
 # List of experiment subdirectories
 experiments = ["control", "Evaluate", "Standard_0", "Standard_1", "Standard_2", "Standard_3" , "Standard_4"]
 
 same_levels = False
 max_smaples = 100
 
-for i, exp in enumerate(experiments):
-    print(exp)
-    cube = load_ensemble(os.path.join(base_dir, exp))*100.0
-    if i == 0:
-        fig, axes = set_up_sow_plot_windows(7, 3, cube, size_scale = 6, flatten = False)
-    # Compute 10th and 90th percentiles
-    annual_average = cube.collapsed('time', iris.analysis.MEAN)
-    p10 = annual_average.collapsed('realization', iris.analysis.PERCENTILE, percent=10)
-    p90 = annual_average.collapsed('realization', iris.analysis.PERCENTILE, percent=90)
-    mean= annual_average.collapsed('realization', iris.analysis.MEAN)
-    p10.rename(f"{exp} - 10th %ile")
-    mean.rename(f"{exp} - mean")
-    p90.rename(f"{exp} - 90th %ile")
-
-    def define_level(cbs):
-        return auto_pretty_levels(cbs, n_levels = 7, ignore_v = 0.0)
+def plot_controls_for_dir(run_dir, 
+                          cmaps = ['gradient_teal', 'gradient_hotpink', 'gradient_red'],
+                          same_levels = False):
+    for i, exp in enumerate(experiments):
+        print(exp)
+        cube = load_ensemble(base_dir, run_dir, exp)*100.0
+        if i == 0:
+            fig, axes = set_up_sow_plot_windows(7, 3, cube, size_scale = 6, flatten = False)
+        # Compute 10th and 90th percentiles
+        annual_average = cube.collapsed('time', iris.analysis.MEAN)
+        p10 = annual_average.collapsed('realization', iris.analysis.PERCENTILE, percent=10)
+        p90 = annual_average.collapsed('realization', iris.analysis.PERCENTILE, percent=90)
+        mean= annual_average.collapsed('realization', iris.analysis.MEAN)
+        p10.rename(f"{exp} - 10th %ile")
+        mean.rename(f"{exp} - mean")
+        p90.rename(f"{exp} - 90th %ile")
+        
+        def define_level(cbs):
+            return auto_pretty_levels(cbs, n_levels = 7, ignore_v = 0.0)
+            
+        if same_levels:        
+            levels =  define_level([p10, p90])
+        else:
+            levels = 'auto'
+        
+        plot_map(p10 , f"{exp} - 10th %ile", cmaps[0], levels, axes[i, 0])   
+        plot_map(mean, f"{exp} - mean", cmaps[1], levels, axes[i, 1])  
+        plot_map(p90 , f"{exp} - 90th %ile", cmaps[2], levels, axes[i, 2])
     
-    if same_levels:        
-        levels =  define_level([p10, p90])
-    else:
-        levels = 'auto'
-    plot_map(p10 , f"{exp} - 10th %ile", 'gradient_teal', levels, axes[i, 0])   
-    plot_map(mean, f"{exp} - mean", 'gradient_hotpink'  , levels, axes[i, 1])  
-    plot_map(p90 , f"{exp} - 90th %ile", 'gradient_red' , levels, axes[i, 2])
-    
-plt.tight_layout()
-fig.savefig(base_dir + "controls.png", dpi=300, bbox_inches="tight")
+    plt.tight_layout()
+    if isinstance(run_dir, list):
+        run_dir = run_dir[1] + run_dir[0]
+    fig.savefig(base_dir + "controls" + run_dir + ".png", dpi=300, bbox_inches="tight")
 
+
+plot_controls_for_dir(run_dirs[0])
+plot_controls_for_dir(run_dirs[1])
+plot_controls_for_dir(run_dirs, 
+                      cmaps = ['diverging_TealOrange'] * 3, same_levels = True)
