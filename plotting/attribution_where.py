00b3b50528f7bf226b246eaad1bfcdb5a1735736
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-06-26 15:58:52 +0100

    using standard constraining and plot function

--- plotting/attribution_where.py
+++ plotting/attribution_where.py
@@ -1,45 +1,53 @@
 import glob
 import os
+import sys
+sys.path.append('libs/')
+from plot_maps import *
+from  constrain_cubes_standard import *
+
+import matplotlib.pyplot as plt
+import iris
+import numpy as np
 
 fact_dir = 'outputs/outputs_scratch/ConFLAME_nrt-attribution9/Amazon-2425/samples/_19-frac_points_0.5/factual-/Evaluate/'
 cfact_dir = 'outputs/outputs_scratch/ConFLAME_nrt-attribution9/Amazon-2425/samples/_19-frac_points_0.5/counterfactual-/Evaluate/'
 
-fact_files = sorted(glob.glob(os.path.join(fact_dir, 'sample-pred*.nc')))[0:100]
-cfact_files = sorted(glob.glob(os.path.join(cfact_dir, 'sample-pred*.nc')))[0:100]
-
-import iris
-import numpy as np
+fact_files = sorted(glob.glob(os.path.join(fact_dir, 'sample-pred*.nc')))[0:1000]
+cfact_files = sorted(glob.glob(os.path.join(cfact_dir, 'sample-pred*.nc')))[0:1000]
 
 # Load first cube to get grid info
-template_cube = iris.load_cube(fact_files[0])
-ny, nx = template_cube.shape[-2:]  # latitude, longitude
-
-# Array to store counts
-count_map = np.zeros((ny, nx), dtype=int)
+count_map = iris.load_cube(fact_files[0])[0]
+count_map.data[:] = 0.0
 
 # Which month index?
-month_idx = 0  # adjust as needed
+month_idx = [0, 1, 2]  # adjust as needed
+year = 2024
+def load_file_month(file):
+    cube = iris.load_cube(file)
+    cube = sub_year_range(cube, [year, year])
+    cube = sub_year_months(cube, month_idx)
+    cube = cube.collapsed('time', iris.analysis.MEAN)    
+    return cube.data
+
 
 for f_file, c_file in zip(fact_files, cfact_files):
-    fact_cube = iris.load_cube(f_file)
-    cfact_cube = iris.load_cube(c_file)
-    
-    # Extract the month slice
-    fact_data = fact_cube.data[month_idx, :, :]
-    cfact_data = cfact_cube.data[month_idx, :, :]
+    fact_data = load_file_month(f_file)
+    cfact_data = load_file_month(c_file)
     
     # Compare and count
-    count_map += (fact_data > cfact_data)
+    count_map.data += (fact_data > cfact_data)
 
-import matplotlib.pyplot as plt
-
-lons = template_cube.coord('longitude').points
-lats = template_cube.coord('latitude').points
-
+count_map.data = count_map.data * 100.0/len(fact_files)
+count_map.data[count_map.data<50.0] = 0.0
+set_trace()
 plt.figure(figsize=(10, 6))
-plt.pcolormesh(lons, lats, count_map, shading='auto', cmap='viridis')
-plt.colorbar(label='Count of Factual > Counterfactual')
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
-plt.title(f'Count map: factual > counterfactual (month index {month_idx})')
+
+plot_map_sow(count_map, "Climates most likely impacts", 
+                    cmap=SoW_cmap['gradient_hues'], 
+                    #levels=[0, 10, 20, 30, 40, 50, 60, 70, 80 ,90, 100],
+                    levels=[50, 70, 80, 90, 95, 99, 100],
+                    extend = 'min', cbar_label = "P(Factual > Counterfactual))")
+#plt.colorbar(label='Count of Factual > Counterfactual')
+
+#   plt.title(f'Count map: factual > counterfactual (month index {month_idx})')
 plt.show()
