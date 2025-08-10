f97e9a3c798f81d826f31abcfc9d51f5736f94af
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2025-08-10 17:30:50 +0100

    small udpate

--- sbatch_metoffice.sh
+++ sbatch_metoffice.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH --mem=400000M
-#SBATCH --ntasks=20                     # Number of nodes
+#SBATCH --ntasks=5                     # Number of nodes
 #SBATCH --output=outputs/ConFLAME_output_%j.txt         # Output file (%j expands to job ID)
 #SBATCH --error=outputs/ConFLAME_error_%j.txt           # Error file (%j expands to job ID)
-#SBATCH --time=6:00:00 
+#SBATCH --time=6:00:00   #SBATCH --partition=cpu-long
 
 # Get the namelist argument
 NAMELIST=$1
