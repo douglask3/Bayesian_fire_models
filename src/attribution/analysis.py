e52d6b827ed572cc0c66f1632add76269b4fcf88
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-01-30 17:51:40 +0000

    moving into a function

--- src/attribution/analysis.py
+++ src/attribution/analysis.py
@@ -8,17 +8,11 @@ from plot_effect_rr import *
 from attribution_where import *
 from plot_BA_climateology import *
 import imageio
+from  pathlib import Path
 
-
-
-if __name__=="__main__":
-    dir1 = "outputs/outputs_scratch/play/"
-    dir2 = "/time_series/_16-frac_points_0.02/" 
-
-    regions = ["Pantanal"]
-
-    obs_dir = 'data/data/driving_data_base//'
-    obs_file = 'burnt_area_data.csv'
+def plot_all_attribution_scatter(dir1, dir2, regions, obs_dir, obs_file):
+    if not isinstance(regions, list):
+        regions = [regions]
 
     def plot_attribution_scatter_generic(counterfactual_name, 
                                          plot_name = "attribution_scatter",
@@ -34,4 +28,14 @@ if __name__=="__main__":
                                      effect_ratio_and_rr_over_range)
     plot_attribution_scatter_generic("counterfactual-metmean", "rr_line",
                                      effect_ratio_and_rr_over_range)
+
+if __name__=="__main__":
+    dir1 = "outputs/outputs_scratch/play/"
+    dir2 = "/time_series/_16-frac_points_0.02/" 
+
+    region = "Pantanal"
+
+    obs_dir = 'data/data/driving_data_base//'
+    obs_file = 'burnt_area_data.csv'  
     
+    plot_all_attribution_scatter(dir1, dir2, region, obs_dir, obs_file)
