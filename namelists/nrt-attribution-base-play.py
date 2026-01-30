f5c5659e398a9755d504e462acc0df8db799e524
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-01-30 14:33:03 +0000

    tidying up

--- namelists/nrt-attribution-base-play.py
+++ namelists/nrt-attribution-base-play.py
@@ -1,6 +1,6 @@
 ## model info
 regions:: ['Pantanal']
-model_title::  'attribution-base-localBA-data-SuperSlimmed-26/<<region>>'
+model_title::  'attribution-base-localBA-data-NEW/<<region>>'
 
 ## input data paths and filenames
 
@@ -16,7 +16,7 @@ y_filen:: "Fire_fraction_Pantanal.nc"
 CA_filen:: None
 
 #Pantanal
-x_filen_list:: ["precip", "max_consec_dry", "tas_max", "hurs_min", "Agriculture_fraction_Pantanal", "Pasture_fraction_Pantanal", "Forest_fraction_Pantanal", "Grassland_fraction_Pantanal", "Savanna_fraction_Pantanal", "TV_CFUEL_CORRECTED_MAP", "rural_population_regridded_to_era5", "urban_area_regridded_to_era5",  "Wetland_fraction_Pantanal", "monthly_cg_strokes", "change_Wetland_fraction_Pantanal.nc", "change_Forest_fraction_Pantanal.nc"] #"roadDensity",
+x_filen_list:: ["precip", "max_consec_dry", "tas_max", "hurs_min", "Agriculture_fraction_Pantanal", "Pasture_fraction_Pantanal", "Forest_fraction_Pantanal", "Grassland_fraction_Pantanal", "Savanna_fraction_Pantanal", "TV_CFUEL_CORRECTED_MAP", "rural_population_regridded_to_era5", "urban_population_regridded_to_era5",  "Wetland_fraction_Pantanal", "monthly_cg_strokes", "change_Wetland_fraction_Pantanal.nc", "change_Forest_fraction_Pantanal.nc"] #"roadDensity",
 #dry_days, hurs_mean and wind
 Y_scale:: 0.01
 
@@ -27,19 +27,19 @@ priors:: {'pname': "link-qSpread_sigma",'np': 1, 'dist': 'HalfNormal', 'sigma': 
 
 control_names:: ['Fuel','Moisture', 'Suppression', 'Ignitions']
 priors:: {'pname': "control_Direction", 'value': [1, 1, 1, 1]}
-priors:: {'pname': "controlID", 'value': [[6, 7, 8, 12, 14], [0, 1, 2,  3, 6, 8, 14], [4, 5, 10, 12], [4, 5, 10, 13]]}
-priors:: {'pname': "driver_Direction", 'value': [[1, 1, 1, 1, 1], [-1, -1, -1, -1, -1, -1, 1], [-1, -1, -1, -1], [1, 1, 1, 1]]}
+priors:: {'pname': "controlID", 'value': [[6, 7, 8, 12, 14], [0, 1, 2,  3, 6, 8, 14], [4, 5, 12], [4, 5, 10, 13]]}
+priors:: {'pname': "driver_Direction", 'value': [[1, 1, 1, 1, 1], [-1, -1, -1, -1, -1, -1, 1], [-1, -1, -1], [1, 1, 1, 1]]}
 priors:: {'pname': "x0",'np': 4, 'dist': 'Normal', 'mu': 0.0, 'sigma': 10.0}
 
 #betas
 priors:: {'pname': "betas",'np': 5, 'dist': 'LogNormal', 'mu': 0.0, 'sigma': 2.0}
 priors:: {'pname': "betas",'np': 7, 'dist': 'LogNormal', 'mu': 0.0, 'sigma': 2.0}
-priors:: {'pname': "betas",'np': 4, 'dist': 'LogNormal', 'mu': 0.0, 'sigma': 2.0}
+priors:: {'pname': "betas",'np': 3, 'dist': 'LogNormal', 'mu': 0.0, 'sigma': 2.0}
 priors:: {'pname': "betas",'np': 4, 'dist': 'LogNormal', 'mu': 0.0, 'sigma': 2.0}
 #powers
 priors:: {'pname': "powers",'np': 5, 'dist': 'LogNormal', 'mu': 1.0, 'sigma': 2.0}
 priors:: {'pname': "powers",'np': 7, 'dist': 'LogNormal', 'mu': 1.0, 'sigma': 2.0}
-priors:: {'pname': "powers",'np': 4, 'dist': 'LogNormal', 'mu': 1.0, 'sigma': 2.0}
+priors:: {'pname': "powers",'np': 3, 'dist': 'LogNormal', 'mu': 1.0, 'sigma': 2.0}
 priors:: {'pname': "powers",'np': 4, 'dist': 'LogNormal', 'mu': 1.0, 'sigma': 2.0}
 
 
@@ -76,7 +76,7 @@ y_filen_eval:: 'Fire_fraction_Pantanal.nc'
 subset_function_eval:: [sub_year_range, sub_year_months]
 subset_function_args_eval:: [{'year_range': [2024, 2026]}, {'months_of_year': [4,5, 6, 7, 8]}]
 plot_drivers :: True
-sample_for_plot:: 50
+sample_for_plot:: 1000
 #if it doenst work delte the sample folder and generate again
 
 plot_control_maps:: True
