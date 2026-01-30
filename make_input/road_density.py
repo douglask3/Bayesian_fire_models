cb0b84267280e77c107327b8b1ea0d38d4f2a88f
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-01-30 14:42:37 +0000

    redoing road denisty maker

--- make_input/road_density.py
+++ make_input/road_density.py
@@ -12,9 +12,9 @@ import numpy as np
 
 import iris.quickplot as qplt
 
-file_path = "/data/users/dkelley/MaxEnt/raw_data/grip4_total_dens_m_km2.asc"
-output_path = "../../ConFIRE_attribute/isimip3a/driving_data/GSWP3-W5E5-20yrs/Brazil/AllConFire_2000_2009/"
-target_cube = iris.load_cube('../data/BR_Biomes.nc')
+file_path = "data/data/roadDensity/grip4_total_dens_m_km2.asc"
+#output_path = "../../ConFIRE_attribute/isimip3a/driving_data/GSWP3-W5E5-20yrs/Brazil/AllConFire_2000_2009/"
+#
 
 coord_sys = cs.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
 
@@ -37,22 +37,32 @@ lon0 = grabVar('xllcorner') + dcell/2
 lon1 = lon0 + grabVar('ncols') * dcell
 
 latitude = iris.coords.DimCoord(np.flip(np.arange(lat0, lat1, dcell)), 
-                                standard_name='latitude', units='degrees')
+                                    standard_name='latitude', units='degrees')
 longitude = iris.coords.DimCoord(np.arange(lon0, lon1, dcell), 
-                                 standard_name='longitude', units='degrees')
-
-
+                                     standard_name='longitude', units='degrees')
 
 # Create a new Iris cube
-
-cube = iris.cube.Cube(data, dim_coords_and_dims=[(latitude, 0), (longitude, 1)],
+cube_rd = iris.cube.Cube(data, dim_coords_and_dims=[(latitude, 0), (longitude, 1)],
                       long_name='Road Density', units='km^2')
-cube.data[cube.data < -10.0] = np.nan
-cube.data[cube.data <   0.0] = 0.0
+cube_rd.data[cube_rd.data < -10.0] = np.nan
+cube_rd.data[cube_rd.data <   0.0] = 0.0
 
-# Regrid the original cube to the target cube's grid
-regridded_cube = cube.regrid(target_cube, iris.analysis.Linear())
+def make_road_density_for_region(region, target_dir, subdir, target_file):
+    dir = target_dir + '/' + region + '/' + subdir + '/'
+    file = dir + target_file
+    #set_trace()
+    target_cube = iris.load_cube(file)
+    # Regrid the original cube to the target cube's grid
+    regridded_cube = cube_rd.regrid(target_cube, iris.analysis.Linear())
 
-iris.save(regridded_cube, output_path + '/' + 'road_density.nc')
-
+    iris.save(regridded_cube, dir + '/' + 'road_density.nc')
+    
+if __name__=="__main__":
+    regions = ['Pantanal', 'Amazon']
+    target_dir = "data/data/driving_data_base/"
+    subdir = "/nrt/era5_monthly/"
+    target_file = "precip.nc"
+    for region in regions:
+        make_road_density_for_region(region, target_dir, subdir, target_file)
+    
 
