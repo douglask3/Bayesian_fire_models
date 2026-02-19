c48cde6667f1735f93c2175efaf6a06ae85fffc9
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-02-19 11:09:37 +0000

    tidying

--- src/futures/futures.py
+++ src/futures/futures.py
@@ -5,13 +5,6 @@ from pdb import set_trace
 from datetime import datetime
 # Define the directory path
 
-hist_path = "outputs/outputs_scratch/Base-ISIMIP_large-4/ConFLAME_Amazon-2425/time_series/_15-frac_points_0.5/historical/GFDL-ESM4-/mean/members/absolute/Evaluate.csv"
-
-obs_path = "data/data/driving_data_base/Amazon/burnt_area_data.csv"
-
-target_mnths = range(5, 7)
-target_years = [2024]
-
 
 def obtain_mnths(dat, target_mnths, target_years):
     try:
@@ -41,6 +34,13 @@ def get_mod_dat_for_years_mnths(path, target_mnths = range(0, 12),
     np.array(dat)
 
 if __name__=="__main__":
+    hist_path = "outputs/outputs_scratch/Base-ISIMIP_large-4/ConFLAME_Amazon-2425/time_series/_15-frac_points_0.5/historical/GFDL-ESM4-/mean/members/absolute/Evaluate.csv"
+
+    obs_path = "data/data/driving_data_base/Amazon/burnt_area_data.csv"
+
+    target_mnths = range(5, 7)
+    target_years = [2024]
+
     obs = pd.read_csv(obs_path)
     which_mnths = obtain_mnths(obs, target_mnths, target_years)
     obs = np.mean(obs['mean_burnt_area'][which_mnths])
