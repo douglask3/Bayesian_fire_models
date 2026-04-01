5010f18d1458c1370534c47942858c7342e871d7
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-04-01 08:13:06 +0100

    plotting sub regions, Pantanal, and using nc file rather tah csv

--- docs/base/BA_season_time_series.r
+++ docs/base/BA_season_time_series.r
@@ -1,19 +1,64 @@
-
+graphics.off()
 
 cols = c("#cfe9ff", "#fc6", "#f68373", "#c7384e", "#862976")
 cols = c("#7a44ff", "#0096a1", "#e98400", "#b50000")
 target_year = 2024
+dir = 'data/data/driving_data_base/'
 
-time = dat$time
 
-plot_BA_type <- function(name, cname, xtitle = '', ytitle = '') {
-    print(par("mar"))
-    BA = dat[,name]/4
-    cBA = dat[,cname]/4  
+open_BA_dat <- function(region, q95) {
+    dir = paste0(dir, region, '/nrt/era5_monthly/')
+    ba_file = list.files(dir, full.names=TRUE)
+    ba_file = ba_file[grepl('Fire_f', ba_file)][1]
+    
+    dat = rast(ba_file)
+    
+    #if (q95) {
+    #    for (i in 1:nlyr(dat)) {
+    #        print(i)
+    #        q = quantile(dat[[i]][], 0.95, na.rm = T)
+    #        dat[[i]][dat[[i]]<q] = NaN
+    #    }
+    #}
+    
+    area = cellSize(dat[[1]])
+    area0 = area
+    area[is.na(dat)]   = NaN
+    tArea = sum(area[], na.rm = TRUE)
+    BA_4_layer <- function(i) {
+        print(i)
+        if (q95) {
+            q = quantile(dat[[i]][], 0.95, na.rm = T)
+            dat[[i]][dat[[i]]<q] = NaN
+            area = area0
+            area[is.na(dat[[i]])]   = NaN
+            tArea = sum(area[], na.rm = TRUE)
+        }
+        sum(values(dat[[i]]*area), na.rm = TRUE)/tArea
+    }
+    BA = sapply(1:nlyr(dat), BA_4_layer)
+    
+    time = time(dat)
+    cBA = sapply(1:12, function(mn) mean(BA[seq(mn, length(BA), by = 12)]))
+    cBA = rep(cBA, length.out = length(BA))
+    return(list(time, BA, cBA))
+}
+
+plot_BA_type <- function(region, q95 = FALSE, xtitle = '', ytitle = '') {
+    
+    BA = open_BA_dat(region, q95)
+    cBA = BA[[3]]
+    time = BA[[1]]
+    BA = BA[[2]]
+    #time = dat$time
+    
+    #BA = dat[,name]/4
+    #cBA = dat[,cname]/4  
     #BA = dBA
     y_range = range(BA)
-    y_max = quantile(BA, 0.999 )/1.3
+    y_max = quantile(BA, 0.999 )/1.6
     #plot(c(0.5, 12.5), y_range, type = 'n', xlab = '', ylab = '', xaxt = 'n', yaxt = 'n')
+    
     plot(0, 0, xlim = c(y_max * c(-1, 1)), ylim = c(y_max * c(-1, 1)), type = 'n', xlab = '', ylab = '', xaxt = 'n', 
          yaxt = 'n', asp = 1, axes = FALSE)#, xaxs = 'i', yaxs = 'i')
 
@@ -69,11 +114,14 @@ plot_BA_type <- function(name, cname, xtitle = '', ytitle = '') {
         test = substr(time, 1, 4) == year
         x = as.numeric(substr(time[test], 6, 7))
         y = BAi[test]
-        if (year > years[1]) {
+        
+        print(paste("year:", year, "\tval:", sum(y)))
+        
+        if (year > years[1] && FALSE) {
             x = c(x[1] - 1, x)
             y = c(BAi[which(test)[1]-1], y)
         }
-        if (year < tail(years, 1) && FALSE) {
+        if (year < tail(years, 1)) {
             x = c(x, tail(x, 1) + 1)
             y = c(y, BAi[tail(which(test), 1)+1])
         }
@@ -86,7 +134,7 @@ plot_BA_type <- function(name, cname, xtitle = '', ytitle = '') {
             lwd = 1
             lty = 3
         }   
-        theta = - 2 * pi * (x-2.5) / 12
+        theta = - 2 * pi * (x-3.5) / 12
     
         # polar → cartesian
         x_cart = y * cos(theta)
@@ -97,7 +145,7 @@ plot_BA_type <- function(name, cname, xtitle = '', ytitle = '') {
     
     mapply(add_year, years, 'black', MoreArgs = list(BAi = cBA))
     mapply(add_year, years, cols, MoreArgs = list(BAi = BA))
-    return(cols)
+    return(list(cols, years))
 }
 
 png("figs/BA_Base_TS.png", height = 9, width = 6, units = 'in', res = 300)
@@ -105,12 +153,17 @@ layout(rbind(t(matrix(1:4, ncol = 2)), 5))
 par( mar = rep(0.9, 4), oma = c(1.8, 1.2, 1.8, 1.2))
 dat = read.csv('data/data/driving_data_base/Amazon/burnt_area_data.csv', 
                stringsAsFactors = FALSE)
-plot_BA_type('mean_burnt_area', 'mean_burnt_area_climateology', 'Amazonia', 'All region')
-plot_BA_type('p95_burnt_area', 'p95_burnt_area_climateology', '', 'Sub-regional extremes')
+plot_BA_type('Amazon', FALSE, 'Amazonia', 'All region')
+
+#plot_BA_type('p95_burnt_area', 'p95_burnt_area_climateology', '', 'Sub-regional extremes')
+plot_BA_type('Amazon', TRUE, '', 'Sub-regional extremes')
 dat = read.csv('data/data/driving_data_base/Pantanal/burnt_area_data.csv', 
                 stringsAsFactors = FALSE)
-plot_BA_type('mean_burnt_area', 'mean_burnt_area_climateology', 'Pantanal', '')
-cols = plot_BA_type('p95_burnt_area', 'p95_burnt_area_climateology', '', '')
+#plot_BA_type('mean_burnt_area', 'mean_burnt_area_climateology', 'Pantanal', '')
+plot_BA_type('Pantanal', FALSE, 'Pantanal', '')
+#cols = plot_BA_type('p95_burnt_area', 'p95_burnt_area_climateology', '', '')
+cols = plot_BA_type('Pantanal', TRUE, '', '')
+years = cols[[2]]; cols = cols[[1]]
 plot.new()
 legs = unique(years)
 lty = rep(3, length(legs))
