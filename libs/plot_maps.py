f0573acc0ad84f945c50764c708e9eb76ef1916f
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-04-29 11:35:24 +0100

    tidyig

--- libs/plot_maps.py
+++ libs/plot_maps.py
@@ -73,7 +73,9 @@ def plot_BayesModel_maps(Sim, levels = None, cmap = 'gradient_reds', ylab = '', 
         #plot_map_sow(cube, plot_name,  cmap = SoW_cmap[cmap], levels=levels, ax=ax, cbar_label = "", **kw, **kw2)
         #set_trace()
         try:
-            plot_annual_mean(cube, levels, cmap, plot_name = plot_name, scale = scale,                      Nrows = Nrows, Ncols = Ncols, plot_n = plot_n + plot0, *args, **kw, **kw2)
+            plot_annual_mean(cube, levels, cmap, plot_name = plot_name, scale = scale,      
+                             Nrows = Nrows, Ncols = Ncols, plot_n = plot_n + plot0, 
+                             *args, **kw, **kw2)
         except:
             set_trace()
         if plot_n == 1:
