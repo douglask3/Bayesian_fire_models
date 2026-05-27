2f2afc15a90d9a55242f3ccca92c820c8de5f280
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-05-27 11:59:14 +0100

    modulising

--- plot_trend_maps.py
+++ plot_trend_maps.py
@@ -15,6 +15,32 @@ experiments = ["control", "Standard_0", "Standard_1", "Standard_2"]
 # Path
 path = "outputs/outputs/model_test_baseline_1/samples/_9-frac_points_0.001/baseline-/"
 
+def plot_trend(p50_cube, prob_cube, name, cmap, add_cbar, levels, axes, axi):
+    
+    if levels is None: 
+        levels = auto_pretty_levels(p50_cube.data, n_levels=3)
+    plot_map_sow(p50_cube, name, cmap = cmap, levels = levels, ax = axes[axi * 2], 
+                 add_cbar = add_cbar)
+    if axi == 0:
+        axes[0].text(
+            0, 0.5, "Trend",
+            transform=axes[0].transAxes,
+            rotation=90,
+            va='center',
+            ha='right'
+        )
+    plot_map_sow(prob_cube, ax = axes[axi * 2 + 1], extend = 'neither', 
+                 levels = [0, 0.01, 0.05, 0.1, 0.9, 0.95, 0.99, 1], add_cbar = add_cbar)
+    if axi == 0:
+        axes[1].text(
+            0, 0.5, "Likelihood > 0",
+            transform=axes[1].transAxes,
+            rotation=90,
+            va='center',
+            ha='right'
+        )
+    return levels
+
 
 def plot_experiment(experiment, axi, cmap, levels, name, fig = None, axes  = None):
     files = sorted(glob.glob(path + experiment + "/sample-pred*.nc"))
@@ -47,6 +73,8 @@ def plot_experiment(experiment, axi, cmap, levels, name, fig = None, axes  = Non
     p95 = np.percentile(diffs, 95, axis=0)
     
     prob_gt0 = np.mean(diffs > 0, axis=0) #* 100  # %
+    prob_is0 = np.mean(diffs == 0, axis=0)
+    prob_gt0 = prob_gt0 + 0.5*prob_is0
     prob_gt0[np.isnan(p50)] = np.nan
     
     template = iris.load_cube(files[0])[0, :, :]  # just lat/lon grid
@@ -64,38 +92,19 @@ def plot_experiment(experiment, axi, cmap, levels, name, fig = None, axes  = Non
     if axes is None:
         fig, axes = set_up_sow_plot_windows(2, len( experiments), p50_cube, transpose = True)   
     
-    if levels is None: 
-        levels = auto_pretty_levels(p50_cube.data, n_levels=5)
 
     if axi == len(experiments) -1:
         add_cbar = True
     else:
         add_cbar = False
-    plot_map_sow(p50_cube, name, cmap = cmap, levels = levels, ax = axes[axi * 2], 
-                 add_cbar = add_cbar)
-    if axi == 0:
-        axes[0].text(
-            0, 0.5, "Trend",
-            transform=axes[0].transAxes,
-            rotation=90,
-            va='center',
-            ha='right'
-        )
-    plot_map_sow(prob_cube, ax = axes[axi * 2 + 1], extend = 'neither', 
-                 levels = [0, 0.01, 0.05, 0.1, 0.9, 0.95, 0.99, 1], add_cbar = add_cbar)
-    if axi == 0:
-        axes[1].text(
-            0, 0.5, "Likelihood > 0",
-            transform=axes[1].transAxes,
-            rotation=90,
-            va='center',
-            ha='right'
-        )
-
+    
+    levels = plot_trend(p50_cube, prob_cube, name, cmap, add_cbar, levels, axes, axi)
+    
     return fig, axes, levels
 
 cmap = SoW_cmap['diverging_GreenPink']
-cmap.reversed()
+cmap = cmap.reversed()
+#set_trace()
 axes = None
 fig = None
 levels = None
