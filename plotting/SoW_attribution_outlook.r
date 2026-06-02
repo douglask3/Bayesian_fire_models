05cbb0e2c925b5932631de7328abb925a5809416
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-06-02 16:08:23 +0100

    plotting af for atribution and projections

--- plotting/SoW_attribution_outlook.r
+++ plotting/SoW_attribution_outlook.r
@@ -1,12 +1,16 @@
 
-openDat <- function(dir, subdir, experiment, area, mnths, years) {
-    file = gsub('<<region>>', gsub(' ', '_', region), dir)
-    file = paste(file, subdir, area, '/members/absolute/', 
-                 paste0(experiment, '.csv'),
-                 sep = '/')
-
-    print(file)
-    dat = read.csv(file)
+openDat <- function(dir, subdirs, experiment, area, mnths, years) {
+    openFile <- function(subdir) {
+        file = gsub('<<region>>', gsub(' ', '_', region), dir)
+        file = paste(file, subdir, area, '/members/absolute/', 
+                     paste0(experiment, '.csv'),
+                     sep = '/')
+        print(file)
+        return(read.csv(file))
+    }
+    dats = lapply(subdirs, openFile)
+    dat = do.call(cbind, dats)
+    
        
     in_time = apply(sapply(years, function(yr)
                         substr(colnames(dat), 2, 5) == as.character(yr)), 1, any)    
@@ -39,7 +43,12 @@ kde <- function(x) {
     })
 }
 
-af_tscale <- function(x) x/(1+x)
+af_tscale <- function(x) {
+    y = x
+    y[x>=1] = 1-1/x[x>=1]
+    y[x<1] = -(1-x[x<1])
+    return((y+1)/2)
+}
 af_iscale <- function(x) x/(1-x)
 
 plot_af <- function(fact, cfact, xpos = 1, col = 'red', name = '', bar = TRUE, label = '') {
@@ -59,8 +68,9 @@ plot_af <- function(fact, cfact, xpos = 1, col = 'red', name = '', bar = TRUE, l
         
     }
     likelihood = round(mean(af>1)*100 + mean(af==1)*50)
-    text(x = xpos - 0.1, y = pcs[3], paste0(likelihood, '%'), adj = 1.1) 
-    text(x = xpos, y = min(pcs), adj = 1.1, srt = 45, name)
+    #text(x = xpos - 0.1, y = pcs[3], paste0(likelihood, '%'), adj = 1.1) 
+    text(x = xpos, y = pcs[1], paste0(likelihood, '%'), adj = c(0.5, 2)) 
+    #text(x = xpos, y = min(pcs), adj = 1.1, srt = 45, name)
 }
 
 regions = c("Midwestern Canadian Shield forests", 
@@ -68,7 +78,9 @@ regions = c("Midwestern Canadian Shield forests",
             "Northwest Iberia")
 
 HadGEM_dir = "outputs/outputs_scratch/SoW2526/attribution-HadGEM-test29-fuelcf4/<<region>>/time_series/_16-frac_points_0.5/"
-ISIMIP_dir = "outputs/outputs_scratch/SoW2526/isimip/full-3/<<region>>/time_series/_15-frac_points_0.5/"
+ISIMIP_dir = "outputs/outputs_scratch/SoW2526/isimip/full-4-notree/<<region>>/time_series/_15-frac_points_0.5/"
+
+gcms = c("GFDL-ESM4-", "IPSL-CM6A-LR-", "MPI-ESM1-2-HR-", "MRI-ESM2-0-", "UKESM1-0-LL-")
 
 region = tail(regions, 1)
 mnths = c('01','02', '03', '04', '05', '06', '07','08', '09', '10', '11', '12')
@@ -76,53 +88,129 @@ mnths = c('09')
 years = c(2025)
 
 
-new_empty_plot_logit <- function(xlim = c(0, 2), ylab =  'Amplifcation factor') {
-    plot(c(0, 2), c(0, 1), xlab = '', ylab = '', type = 'n', yaxt = 'n', yaxs = 'i')
+new_empty_plot_logit <- function(xlim = c(0, 2), ylab =  'Amplifcation factor',
+                                 add_xlabs = ylab != '', ...) {
+    plot(xlim,  c(0, 1), xlab = '', ylab = '', type = 'n', yaxt = 'n', yaxs = 'i', 
+         xaxt = 'n', ...)
     
     labels = c(0, 1/128, 1/64, 1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4, 8, 16, 32, 64, 128, 1000000)
     at = af_tscale(labels)
-    labels = c('0', '', '', '', '', '', '1/4', '1/2', 'no\nchange', '2', '4', '', '', '', '', '', 'All from\nclimate')
+    if (add_xlabs) 
+        labels = c('0', '', '', '', '', '', '1/4', '1/2', 'no\nchange', 
+                   '2', '4', '', '', '', '', '', 'All from\nclimate')
+    else
+        labels = rep('', length(labels))
     axis(2, at = at, labels = labels)
-    mtext(side = 2, line = 2.75, ylab)
-}
+    mtext(side = 2, line = 3, ylab)
+    for (y in at)
+        lines(c(-9E9, 9E9), c(y, y), col= 'grey', lty = 2)
     
-plot_region <- function(region, HadGEM_dir, ISIMIP_dir, mnths, years) {
+    if (ylab == '') {   
+        mtext(side =4, line = 3, "% explained by climate change")
+        axis(4, seq(0, 1, length.out=  9), seq(-100, 100,length.out=  9))
+    }
+}
+
+att_af_calc <- function(dir, factual_name, cfactual_name, exp, mnths, years,
+                        BA, xp, xoffset, samples = NULL, ...) {
+    
+    fact = openDat(dir, factual_name, exp, "mean", mnths, years)
+    cfact = openDat(dir, cfactual_name, exp, "mean", mnths, years)
+    BA = 0.1
+        
+    if (is.null(samples)) {
+        prob = exp(BA*log(fact) + (1.0-BA)*log((1-fact)))
+        samples = sample(1:length(prob), 1000, TRUE, prob)
+    }
+    facts = fact[samples]
+    cfacts = cfact[samples] 
+    plot_af(facts, cfacts, xp + xoffset, ...)
+    return(samples)
+}
+
+futr_af_calc <- function(dir, factual_name, cfactual_name, exp, mnths, years,
+                         BA, xp, xoffset, samples = NULL, name = name, col = col, width = 0.05, 
+                        ...) {
+    
+    if (exp == "Evaluate") {
+        if (factual_name[2] == "ssp370") {
+            polygon(width/2 + c(xoffset, xoffset + 0.3)[c(1, 1, 2, 2)], c(0, 1, 1, 0),
+                     col = '#00000011', border = NA)
+            text(xp + xoffset, 0, adj = c(0.5, -0.3), 
+                 paste0(range(years[[2]]), collapse = ' - '), font = 2)
+        } else if (factual_name[2] == "ssp585")  {         
+            polygon(width/2 + c(xoffset, xoffset + 0.3)[c(1, 1, 2, 2)], c(0, 1, 1, 0),
+                     col = '#00000022', border = NA)
+            lines(rep(width/2 + xoffset + 0.3, 2), c(-9E9, 9E9), lty = 2)
+        }
+       
+    }
+    if (years[[2]][1]== yearss[[1]][[2]][1] && exp == "Evaluate") 
+        text(xoffset + xp/2, 0, adj = c(-1, 0.5), srt = 90, factual_name[2])
+    
+    xs = xoffset + xp/3 + width*0.5*c(-1, 1)
+    for_gcm <- function(gcm) {
+        fact = openDat(dir, paste0(factual_name, '/', gcm), exp, "mean", mnths, years[[1]])
+        cfact = openDat(dir,  paste0(cfactual_name, '/', gcm), exp, "mean", mnths, years[[2]])
+        qt =  mean(fact <= BA)
+        
+        af = quantile(cfact,qt)/quantile(fact,qt)
+        lines(xs, rep(af_tscale(af), 2), lwd = 1, col = col)
+        return(af)
+    }
+    afs = sapply(gcms, for_gcm)
+    polygon(xs[c(1, 1, 2, 2)], af_tscale(range(afs)[c(1,2,2,1)]), 
+            border = NA, col = paste0(col, "66"))
+    #browser()
+    
+    
+}
+plot_region <- function(region, HadGEM_dir, ISIMIP_dir, mnths, years, yearss) {
     
     add_run <- function(dir, factual_name = "factual-", cfactual_name = "counterfactual-",
-                        xoffset = 0.0, years = 2025, mnths = NULL) {
+                        xoffset = 0.0, years = 2025, mnths = NULL, 
+                        plot_FUN = att_af_calc) {
+        
         add_experiemtnt <- function(exp = "Evaluate", col = 'red', xp = 0.25, 
                                     name = 'Burned Areas',
                                     samples = NULL) {
             
-            fact = openDat(dir, factual_name, exp, "mean", mnths, years)
-            cfact = openDat(dir, cfactual_name, exp, "mean", mnths, years)
-
-            BA = 0.1
-        
-            if (is.null(samples)) {
-                prob = exp(BA*log(fact) + (1.0-BA)*log((1-fact)))
-                samples = sample(1:length(prob), 1000, TRUE, prob)
-            }
-            facts = fact[samples]
-            cfacts = cfact[samples] 
-            #browser()
-            plot_af(facts, cfacts, xp + xoffset, name, col = col)
+            samples = plot_FUN(dir, factual_name, cfactual_name, exp, mnths, years,
+                        BA, xp, xoffset, samples, name = name, col = col)
             return(samples)
         } 
         samples = add_experiemtnt(col = "#E98400")
         exps = c("standard-Fuel", "standard-Moisture")#, "standard-Ignition","standard-Suppression")
-        cols = c("#0096A1", "#EE0074")#, "purple", "gret")
+        cols = c("#0096A1", "#EE0074")#, "purple", "grey")
         mapply(add_experiemtnt, exps, cols, c(0.5, 0.75), c("Fuel connectivity", "Dryness"),
                MoreArgs = list(samples = samples))
     }
-    par(mfrow = c(2, 2))
-    new_empty_plot_logit()
+    layout(rbind(1:3, 4:6), widths = c(0.2, 0.02, 0.8))
+    par(oma = c(2, 5, 2, 5), mar = c(1, 0, 1, 0))
+    new_empty_plot_logit(xlim = c(0, 3))
     add_run(HadGEM_dir, mnths = mnths)
-    add_run(ISIMIP_dir, xoffset = 1, years = 2005:2014)
-    text(x = 0.5, y = 0, adj = c(0.5, -0.3), font = 2, 'HadGEM3-A')
-    text(x = 1.5, y = 0, adj = c(0.5, -0.3), font = 2, 'ISIMIP3a')
-
-    new_empty_plot_logit(xlim = c(0, 9))
+    add_run(HadGEM_dir, mnths = mnths, cfactual_name = "counterfactual_mean-", xoffset = 1)
+    add_run(ISIMIP_dir, xoffset = 2, years =  2002:2019)
+    text(x = 0.5, y = 0, adj = c(0.5, -0.3), font = 2, 'HadGEM3-A\nfull ensemble')
+    text(x = 1.5, y = 0, adj = c(0.5, -0.3), font = 2, 'HadGEM3-A\nensemble mean')
+    text(x = 2.5, y = 0, adj = c(0.5, -0.3), font = 2, 'ISIMIP3a')
+    plot.new()
+    new_empty_plot_logit(xlim = c(0, length(yearss)), ylab = '', xaxs = 'i')
+    for_ssp <- function(ssp, xmini_off) {
+        subdir = c("historical", ssp)
+        
+        for_yrss <- function(yrss, xoffset) {
+            add_run(ISIMIP_dir, xoffset = xoffset-1 + xmini_off, years =  list(2010:2019,yrss), 
+                    mnths = mnths,
+                    plot_FUN = futr_af_calc, factual_name = subdir, cfactual_name = subdir)
+        }
+        mapply(for_yrss, yearss, 1:length(yearss))
+    }
+    mapply(for_ssp, c("ssp126", "ssp370", "ssp585"), c(0, 0.3, 0.6))
     browser()
 }
-plot_region(region,  HadGEM_dir, ISIMIP_dir, mnths, years)
+
+yearss = list(2020:2029, 2030:2039, 2040:2049, 2050:2059, 2060:2069, 
+                      2070:2079, 2080:2089, 2090:2099)
+yearss = list(2030:2039, 2040:2049, 2090:2099)
+plot_region(region,  HadGEM_dir, ISIMIP_dir, mnths, years, yearss = yearss)
