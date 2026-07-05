70ec85074f0d6c5f7407a92621a835fb3978db0f
Douglas Kelley <douglas.kelley@metoffice.gov.uk>
2026-07-05 09:54:06 +0100

    needed for obs time series calculation

--- libs/climtatology_difference.py
+++ libs/climtatology_difference.py
@@ -1,9 +1,14 @@
 import numpy as np
+from pdb import set_trace
 
 def climtatology_difference(data, nmths = 12):
 
     # `data` is your (n x m) array
-    n, m = data.shape
+    if data.ndim == 1:
+        m = len(data)
+        n = 1
+    else:
+        n, m = data.shape
 
     # Step 1: Calculate month index for each column (0=Jan, 11=Dec)
     month_indices = np.arange(m) % nmths  # shape (m,)
@@ -13,15 +18,23 @@ def climtatology_difference(data, nmths = 12):
     
     # Step 3: Fill in the monthly means
     for month in range(nmths):
-        climatology[:, month] = data[:, month_indices == month].mean(axis=1)
+        if data.ndim == 1:
+            climatology[0, month] = data[month_indices == month].mean()
+        else:
+            climatology[:, month] = data[:, month_indices == month].mean(axis=1)
 
     # Step 4: Create anomaly and ratio arrays (n x m)
-    anomaly = np.zeros_like(data)
-    ratio = np.zeros_like(data)
+    anomaly = np.zeros((n, m))
+    ratio = np.zeros((n, m))
     
     for i in range(m):
         month = month_indices[i]
-        anomaly[:, i] = data[:, i] - climatology[:, month]
-        ratio[:, i] = data[:, i] / climatology[:, month]
+        if data.ndim == 1:
+            anomaly[0, i] = data[i] - climatology[0, month]
+            ratio[0, i] = data[i] / climatology[0, month]
+        else:
+            anomaly[:, i] = data[:, i] - climatology[:, month]
+            ratio[:, i] = data[:, i] / climatology[:, month]
+    
     return climatology, anomaly, ratio
 
